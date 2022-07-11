e49ca1d7cf955ad50fe9826ce3d9cafe0d51c22f
Petr0v21 <petr0v21@gmail.com>
2022-07-11 18:01:50 +0300

    Corect buttons and add pills

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import { Bars } from "./components/Bars";
+import { useState } from "react";
 import { Buttons } from "./components/Buttons";
-import { Dropdown } from "./components/Dropdown";
 import { Inputs } from "./components/Inputs";
-import { NavBar } from "./components/NavBar";
-import { Select } from "./components/Select";
-import { Tags } from "./components/Tags";
+// import { Dropdown } from "./components/Dropdown";
+// import { NavBar } from "./components/NavBar";
+// import { Select } from "./components/Select";
+// import { Tags } from "./components/Tags";
+// import { Bars } from "./components/Bars";
 import "./styles/App.css";
 
 function App() {
+  const [thema, setThema] = useState("Light");
   return (
     <div className="App">
-      <Buttons />
+      <Buttons thema={thema} />
       <Inputs />
-      <div className="drop">
+      {/* <div className="drop">
         <Dropdown />
         <Select />
         <Tags />
       </div>
       <NavBar />
-      <Bars />
+      <Bars /> */}
     </div>
   );
 }
--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -1,15 +1,27 @@
 import React from "react";
 import styled from "styled-components";
 import "../styles/Buttons.css";
+import icon from "../accets/Image.png";
+import arrow from "../accets/ArrowDown.png";
 
 type ButtonProps = {
+  margin?: string;
   design?: string;
   backcolor?: string;
+  backcolorHover?: string;
+  backcolorActive?: string;
+  borderHover?: string;
+  borderActive?: string;
   radiusBorder?: string;
   widthMax?: string;
   widthMin?: string;
   fontSize?: string;
   slowed?: string;
+  contentBefore?: string;
+  contentAfter?: string;
+  size?: string;
+  spaceBetween?: string;
+  dropdown?: string;
 };
 
 const FormButtonsSmall = styled.form`
@@ -18,14 +30,18 @@ const FormButtonsSmall = styled.form`
 `;
 
 const ButonLink = styled.div`
-  max-width: 300px;
-  min-width: 150px;
+  max-width: 480px;
+  // min-width: 60px;
+  // height: 44px;
+  display: flex;
+  gap: 6px;
   text-align: center;
-  padding: 12px 14px;
-  border-radius: 8px;
+  align-items: center;
+  padding: 12px 16px;
+  border-radius: 12px;
   border: 2px solid #ffffff;
   margin: 5px;
-  color: whitesmoke;
+  color: white;
   cursor: pointer;
   font-size: 14px;
   background-color: #bdc3c7;
@@ -39,7 +55,7 @@ const ButonLink = styled.div`
   }
 `;
 
-const ButtonAdaption = styled(ButonLink)<ButtonProps>`
+const Button = styled(ButonLink)<ButtonProps>`
   ${(props) => {
     switch (props.design) {
       case "success":
@@ -77,6 +93,57 @@ const ButtonAdaption = styled(ButonLink)<ButtonProps>`
     `;
     }
   }}
+  ${(props) => {
+    switch (props.size) {
+      case "small":
+        return `
+          gap: 4px;
+          padding: 8px 12px;
+          font-size: 12px;
+          border-radius: 10px;
+          img {
+            height: 16px;
+          }
+      `;
+      case "medium":
+        return `
+          gap: 6px;
+          padding: 12px 16px;
+          font-size: 14px;
+          border-radius: 14px;
+          img {
+            height: 20px;
+          }
+    `;
+      case "large":
+        return `
+          gap: 6px;
+          padding: 16px 20px;
+          font-size: 16px;
+          border-radius: 16px;
+          img {
+            height: 24px;
+          }
+    `;
+    }
+  }}
+  ${(props) => {
+    switch (props.dropdown) {
+      case "small":
+        return `
+          gap: 32px;
+      `;
+      case "medium":
+        return `
+          gap: 40px;
+    `;
+      case "large":
+        return `
+          gap: 48px;
+    `;
+    }
+  }}
+  gap: ${(props) => (props.spaceBetween ? props.spaceBetween : "")};
   color: ${(props) => (props.color ? props.color : "whitesmoke")};
   background-color: ${(props) => (props.backcolor ? props.backcolor : "")};
   border-radius: ${(props) => (props.radiusBorder ? props.radiusBorder : "")};
@@ -84,19 +151,75 @@ const ButtonAdaption = styled(ButonLink)<ButtonProps>`
   min-width: ${(props) => (props.widthMin ? props.widthMin : "")};
   font-size: ${(props) => (props.fontSize ? props.fontSize : "")};
   transition: all ${(props) => (props.slowed ? props.slowed : "")}s ease;
+  margin: ${(props) => (props.margin ? props.margin : "5px")};
+  &:before {
+    content: ${(props) => props.contentBefore ?? ""};
+  }
+  &:after {
+    content: ${(props) => props.contentAfter ?? ""};
+  }
+  &:hover {
+    background-color: ${(props) => props.backcolorHover ?? ""};
+    border: ${(props) => props.borderActive ?? ""};
+  }
+  &:active {
+    background-color: ${(props) => props.backcolorActive ?? ""};
+    border: ${(props) => props.borderActive ?? ""};
+  }
 `;
 
-export const Buttons: React.FC = (props) => {
+const Pills = styled(Button)<ButtonProps>`
+  padding: 10px 16px;
+  border-radius: 22px;
+  ${(props) => {
+    switch (props.size) {
+      case "small":
+        return `
+        font-size: 12px;
+        padding: 8px 12px;
+        border-radius: 20px;
+    `;
+      case "medium":
+        return `
+        padding: 10px 16px;
+        border-radius: 22px;
+        
+  `;
+      case "large":
+        return `
+        padding: 12px 20px;
+        border-radius: 24px;
+  `;
+    }
+  }}
+`;
+
+export const Buttons: React.FC<{ thema?: string }> = (props) => {
   return (
     <div>
       <h2>Buttons</h2>
       <FormButtonsSmall>
-        <ButtonAdaption design="success">SuccessButton</ButtonAdaption>
-        <ButtonAdaption design="warning">WarningButton</ButtonAdaption>
-        <ButtonAdaption design="danger">DangerButton</ButtonAdaption>
-        <ButtonAdaption color="black" backcolor="">
-          Adaption
-        </ButtonAdaption>
+        <Button>Label</Button>
+        <Button size="small">
+          <img src={icon} alt="icon" />
+          Label
+        </Button>
+        <Button size="small" dropdown="small">
+          Label
+          <img src={arrow} alt="arrow" />
+        </Button>
+      </FormButtonsSmall>
+      <h2>Pills</h2>
+      <FormButtonsSmall>
+        <Pills>Label</Pills>
+        <Pills size="small">
+          <img src={icon} alt="icon" />
+          Label
+        </Pills>
+        <Pills size="small" dropdown="small">
+          Label
+          <img src={arrow} alt="arrow" />
+        </Pills>
       </FormButtonsSmall>
     </div>
   );
